6b06a9986289a7544be7c18f2a2d50e4055fe09c
braharalex <uljador@gmail.com>
2024-02-16 01:50:33 +0200

    first edits

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let intervalId = 0;
 
+startBtn.addEventListener('click', onStartChangeColor);
+stopBtn.addEventListener('click', onStopChangeColor);
+
+function onStartChangeColor() {
+  if (startBtn.hasAttribute('disabled')) {
+    startBtn.removeAttribute('disabled');
+
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+      console.log('kkkkk');
+    }, 1000);
+    console.log('jjjj');
+  }
+
+  startBtn.setAttribute('disabled', '');
+}
+
+function onStopChangeColor() {
+  startBtn.removeAttribute('disabled');
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
