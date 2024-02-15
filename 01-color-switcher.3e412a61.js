90ab28ba4f1808e6d31c9af1eea7fae36bffa692
braharalex <116018690+braharalex@users.noreply.github.com>
2024-02-15 23:50:56 +0000

    Deploying to gh-pages from @ braharalex/my-new-goit-js-hw-09@6b06a9986289a7544be7c18f2a2d50e4055fe09c 🚀

---
+++ 01-color-switcher.3e412a61.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let o=0;t.addEventListener("click",(function(){t.hasAttribute("disabled")&&(t.removeAttribute("disabled"),o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,console.log("kkkkk")}),1e3),console.log("jjjj"));t.setAttribute("disabled","")})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.3e412a61.js.map
